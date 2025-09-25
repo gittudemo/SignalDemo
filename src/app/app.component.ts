41dd79ee3b6f51466594690462910c2463407210
qadir <aqadir1989@gmail.com>
2025-09-25 15:08:02 +0530

    updated-code.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, computed, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'SignalDemo';
+
 }
